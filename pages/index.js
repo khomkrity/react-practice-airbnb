a10a164adb5fa9c18b506c1472c8b91752994c91
omekrit <khomkrit.y@gmail.com>
2021-09-15 12:03:22 +0700

    add banner

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 export default function Home() {
@@ -9,7 +10,12 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      <h1>Hello World</h1>
+      <Banner />
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   );
 }

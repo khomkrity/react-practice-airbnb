c0be2061432e5a668a9733fa650c8be70bfc3c7d
omekrit <khomkrit.y@gmail.com>
2021-09-15 14:06:24 +0700

    add explore card component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import ExploreCard from '../components/ExploreCard';
 
-export default function Home() {
+export default function Home({ exploreData }) {
+  const renderExploreData = exploreData.map(({ img, location, distance }) => {
+    return <ExploreCard key={location} img={img} location={location} distance={distance} />;
+  });
+
   return (
     <div className=''>
       <Head>
@@ -14,8 +19,22 @@ export default function Home() {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {renderExploreData}
+          </div>
         </section>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://links.papareact.com/pyp');
+  const exploreData = await response.json();
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }

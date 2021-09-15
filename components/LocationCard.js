547b63a8353457a6a5ed255d2f5fffc7f6b6e37a
omekrit <khomkrit.y@gmail.com>
2021-09-15 14:24:20 +0700

    reaname location card component

--- components/LocationCard.js
+++ components/LocationCard.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-function ExploreCard({ img, location, distance }) {
+function LocationCard({ img, location, distance }) {
   return (
     <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
       <div className='relative h-16 w-16'>
@@ -14,4 +14,4 @@ function ExploreCard({ img, location, distance }) {
   );
 }
 
-export default ExploreCard;
+export default LocationCard;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
-import ExploreCard from '../components/ExploreCard';
+import LocationCard from '../components/LocationCard';
+import HomeCard from '../components/HomeCard';
 
-export default function Home({ exploreData }) {
-  const renderExploreData = exploreData.map(({ img, location, distance }) => {
-    return <ExploreCard key={location} img={img} location={location} distance={distance} />;
+export default function Home({ locationData, homeData }) {
+  const renderNearbyLocationData = locationData.map(({ img, location, distance }) => {
+    return <LocationCard key={location} img={img} location={location} distance={distance} />;
+  });
+
+  const renderHomeData = homeData.map(({ img, title }) => {
+    return <HomeCard key={title} img={img} title={title} />;
   });
 
   return (
@@ -20,8 +25,12 @@ export default function Home({ exploreData }) {
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {renderExploreData}
+            {renderNearbyLocationData}
           </div>
+        </section>
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          {renderHomeData}
         </section>
       </main>
     </div>
@@ -29,12 +38,15 @@ export default function Home({ exploreData }) {
 }
 
 export async function getStaticProps() {
-  const response = await fetch('https://links.papareact.com/pyp');
-  const exploreData = await response.json();
+  const locationDataResponse = await fetch('https://links.papareact.com/pyp');
+  const homeDataResponse = await fetch('https://links.papareact.com/zp1');
+  const locationData = await locationDataResponse.json();
+  const homeData = await homeDataResponse.json();
 
   return {
     props: {
-      exploreData,
+      locationData,
+      homeData,
     },
   };
 }

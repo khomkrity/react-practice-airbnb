34da18efba3ad9aabb72b57cbc29e6cc3b2ad292
omekrit <khomkrit.y@gmail.com>
2021-09-22 21:13:34 +0700

    add main components and some static button

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,26 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
 function search() {
   return (
-    <div>
-      <h1>I am the search page</h1>
+    <div className='h-screen'>
+      <Header />
+
+      <main className='flex'>
+        <section>
+          <p className='text-xs'>300+ Stays for 5 number of guests</p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+          <div className=''>
+            <p className='button'>Cancellation Flexibility</p>
+            <p className='button'>Type of Place</p>
+            <p className='button'>Price</p>
+            <p className='button'>Rooms of Beds</p>
+            <p className='button'>More Filters</p>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
     </div>
   );
 }

2e02f8d148a977056aa4f81f1ecdcb65ae150cc8
omekrit <khomkrit.y@gmail.com>
2021-09-28 21:10:13 +0700

    decrease picture size

--- components/HomeCard.js
+++ components/HomeCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 function HomeCard({ img, title }) {
   return (
     <div className='cursor-pointer hover:scale-105 transition transform duration-300 ease-out'>
-      <div className='relative h-80 w-80'>
+      <div className='relative h-72 w-72'>
         <Image className='rounded-xl' src={img} layout='fill' />
       </div>
       <h3 className='text-2xl mt-3'>{title}</h3>
